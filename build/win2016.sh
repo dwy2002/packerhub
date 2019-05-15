b18a6d9be7a8780a5b51630239ca0dcf5aa2956b
William Du <dwy2002@hotmail.com>
2019-05-15 11:19:32 +0800

    Update win2016.sh

--- build/win2016.sh
+++ build/win2016.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "download packer tool"
+sudo wget https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip
+sudo unzip packer_1.2.5_linux_amd64.zip
+sudo cp packer /usr/local/sbin/
+sudo cp packer /usr/local/bin
 echo "start packer build"
 cd packer.io
 pwd
